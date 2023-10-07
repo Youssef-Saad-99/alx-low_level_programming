7198d3090aec364d778807e88a594acee10db9ee
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-07 08:02:17 -0700

    Modified

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,14 +14,9 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch != 'e' || ch != 'q')
-		{
-			putchar(ch);
-		}
-		else
-		{
-			continue;
-		}
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
 		ch++;
 	}
 	putchar('\n');
