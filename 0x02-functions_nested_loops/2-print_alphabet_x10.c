ceaa5f8de731d73607378d19e527a7f7743da3fe
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-10 08:35:51 -0700

    Added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 #include "main.h"
 /**
  * print_alphabet_x10 - Entry point
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ *
+ * Description: A function that checks for alphabetic character
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
