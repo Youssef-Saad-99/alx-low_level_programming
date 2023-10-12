329832ce168f1e4241fa9f9d7f479ad4e07e2ae5
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-12 02:11:16 -0700

    Added

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -13,6 +13,6 @@
 int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
-		reutrn (1);
+		return (1);
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: A function that multiplies two integers
+ *
+ * @a: firist input
+ * @b: second input
+ *
+ * Return: multiply of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
