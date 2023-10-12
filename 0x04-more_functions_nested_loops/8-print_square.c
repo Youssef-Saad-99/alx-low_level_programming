5e8b18be9d3e61c31fb042d9ca1e4adc5ac948cf
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-12 06:38:52 -0700

    Added

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ *
+ * Description: A function that prints a square
+ *
+ * @size: takes integer number input
+*/
+
+void print_square(int size)
+{
+	int j, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
