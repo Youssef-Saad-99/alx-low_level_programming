b64527e681374796c99b9daef9b5b4f64ed48885
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-12 02:55:42 -0700

    Added

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.c"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: A function that prints the numbers, from 0 to 9
+*/
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+		{
+			continue;
+		}
+		_putchar(num);
+	}
+	_putchar('\n');
+}
