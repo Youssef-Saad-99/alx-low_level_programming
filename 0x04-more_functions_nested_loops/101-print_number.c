814784de6c7d02cc9da4cf13d470726272abc39e
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-12 07:51:04 -0700

    Added

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ *
+ * Description: A function that prints an integer
+ *
+ * @n: takes integer number input
+*/
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print-number(nmm / 10);
+	_putchar((num % 10) + 48);
+}
