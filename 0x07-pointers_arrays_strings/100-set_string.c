fc80740619f8c84086384512254d41750a76a1a1
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-23 02:21:20 -0700

    Added

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - A function that sets the value of a pointer to a char.
+ *
+ * @s: pointer input
+ * @to: pointer input
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strstr - A function that locates a substring.
+ *
+ * @haystack: Main string input
+ * @needle: Substring input
+ *
+ * Return: the beginning of the located substring,
+ *	or NULL if the substring is not found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return ((char *)haystack);
+	}
+	while (*haystack != '\0')
+	{
+		const char *h = haystack;
+		const char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return ((char *)haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
