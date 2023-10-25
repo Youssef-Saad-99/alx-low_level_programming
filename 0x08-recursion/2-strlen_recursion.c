18295011405723f5eb498db92cf5a3774b76d3f2
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-25 01:09:50 -0700

    Added

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int n;
+	int n = 0;
 
 	if (*s > '\0')
 	{
-		n = n + _strlen_recursion(s + 1) + 1;
+		n += _strlen_recursion(s + 1) + 1;
 	}
 	return (n);
 }
