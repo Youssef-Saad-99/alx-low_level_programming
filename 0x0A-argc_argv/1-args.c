d4c7de3292e0f13db1f06f287289be9dc9a585f8
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-27 16:13:08 -0700

    Added

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,8 @@
  * Return: Always (0) Success
 */
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
