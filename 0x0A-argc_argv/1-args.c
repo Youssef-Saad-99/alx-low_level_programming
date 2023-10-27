ecf3985871ae25f8f7333f9e23afc5804df061a4
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-10-27 15:34:22 -0700

    Added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always (0) Success
+*/
+
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
