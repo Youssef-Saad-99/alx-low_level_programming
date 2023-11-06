eef19c17826bda5430ec9344785d2923fb49a5e3
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-11-06 06:50:44 -0800

    Added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure
+ *
+ * @name: char
+ * @age: integer
+ * @owner: char
+*/
+
 struct dog
 {
 	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
