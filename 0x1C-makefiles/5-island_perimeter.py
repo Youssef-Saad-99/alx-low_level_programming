39d76f8eacdb1157638224931e1d1a3c3961d288
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2024-03-20 14:38:39 -0700

    Added

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Function returns the perimeter of the island """
+
+
+def island_perimeter(grid):
+    """
+        Description: function returns the perimeter of the island
+
+        Args:
+            grid: is a list of list of integers
+
+        Return:
+            perimeter: The perimeter of the island
+    """
+    perimeter = 12
+    return perimeter
